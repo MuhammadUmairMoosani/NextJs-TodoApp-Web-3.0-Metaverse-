3cc22aeb3d878bd234c674d1ee60b3f0424fc1dc
muhammadumairmoosani <muhammadumairmoosani@gmail.com>
2023-02-06 00:42:12 +0500

    Complete Todo List app

---
+++ src/pages/index.tsx
@@ -0,0 +1,65 @@
+import React,{useState} from 'react';
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Todo() {
+  
+  type User =  [{id:number,value:string,done:boolean}] | [];
+
+  const [todoList,setTodoList] = useState<User>([])
+  const [todo,setTodo] = useState('');
+
+  const deleteTodoItem =(index:number) => {
+    const preList = todoList;
+    preList.splice(index,1);
+    setTodoList([...preList]);
+  }
+
+  const finishNUnfinishItem = (index:number) => {
+    const preList = todoList;
+    preList[index].done = !preList[index].done;
+    setTodoList([...preList])
+  }
+
+  const addItem = () => {
+    if(todo) {
+      const preList: User = todoList;
+      const data = {
+        id:preList.length + 1,
+        value:todo,
+        done:false
+      }
+      preList.unshift(data);
+      setTodo('');
+      setTodoList(preList);
+    } 
+  }
+  
+
+  return (
+    <>
+      <Head>
+        <title>Todo App</title>
+      </Head>
+      <main 
+      className={styles.main}
+      >
+        <div style={{width: '100%'}}>
+        <h1 className={styles.heading}>TODO LIST</h1>
+        <div style={{display: 'flex'}}>
+        <input className={styles.input} value={todo} placeholder="Enter todo list item" onChange={(value) => setTodo(value.target.value)} />
+        <button className={styles.addBtn} onClick={addItem} disabled={todo === null}>Add</button>
+        </div>
+        <ul>
+        {todoList.map((item,index) => 
+        <li key={`${index + item.value}`} style={{display: 'flex',alignItems:"center"}}>
+        <h3 className={styles.todoItem} style={{ textDecorationLine: item.done ? "line-through" :""}}>{item.value}</h3>
+        <button className={styles.finishBtn}  onClick={() => finishNUnfinishItem(index)} style={{backgroundColor: item.done ? "#858796" : "#1cc88a", borderColor:  item.done     ? "#858796" : "#1cc88a"}}>{item.done ? "Unfinish" : "Finish" } </button>
+        <button className={styles.deleteBtn} onClick={() => deleteTodoItem(index)}>Delete</button></li>
+         )}
+         </ul>
+        </div>
+      </main>
+    </>
+  )
+}
